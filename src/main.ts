2fc6446716e073b11a201814c6cd1807af5a7739
Bret Hudson <bret@brethudson.com>
2026-09-20 23:47:20 -0700

    Remove space changing color palette

--- src/main.ts
+++ src/main.ts
@@ -239,7 +239,7 @@ async function setupApp(): Promise<void> {
 			debugInfo.classList.toggle('visible');
 			game.debugEnabled = !game.debugEnabled;
 		}
-		game.swapPalette ||= controllerInput.keyPressed('Select') ? 1 : 0;
+		// game.swapPalette ||= controllerInput.keyPressed('Select') ? 1 : 0;
 
 		if (game.swapPalette !== 0) {
 			render.nextPalette(renderer, game.swapPalette);
